c34865042675ce91d0bddf3aab8fa7877f05515a
Denis Tolochko <denis.t@t3leads.com>
2016-02-07 05:16:25 -0800

    Change on slider

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function header() {
+
+}
+
+
+function div_navigation() { ?>
+    <div class="navigation">
+        <div id="smoothmenu1" class="ddsmoothmenu">
+            <ul>
+                <li class="current-menu-item"><a href="index.php">Home</a></li>
+                <li><a href="quote.html">Quote</a></li>
+                <li><a href="send-a-drive.html">Send a Drive</a></li>
+                <li><a href="advice-and-price.html">Advice and Price</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+        <div class="announcment">
+            <ul>
+                <li>
+                    <h5 class="bold"><a href="blogdetail.html" class="txthover">Consectetur adipiscing elit. Nulla</a></h5>
+                    <p class="date">December 18th 2011</p>
+                </li>
+                <li>
+                    <h5 class="bold"><a href="blogdetail.html" class="txthover">Consectetur adipiscing elit. Nulla</a></h5>
+                    <p class="date">December 18th 2011</p>
+                </li>
+                <li>
+                    <h5 class="bold"><a href="blogdetail.html" class="txthover">Consectetur adipiscing elit. Nulla</a></h5>
+                    <p class="date">December 18th 2011</p>
+                </li>
+            </ul>
+            <a href="#" class="mlink">Announcements</a>
+        </div>
+    </div>
+<?php }
+
+
+function footer() {
+
+}
+
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include_once(./functions.php);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -72,35 +73,7 @@
             </div>
             <div class="clear"></div>
         </div>
-        <div class="navigation">
-        	<div id="smoothmenu1" class="ddsmoothmenu">
-                <ul>
-                    <li class="current-menu-item"><a href="index.php">Home</a></li>
-                    <li><a href="quote.html">Quote</a></li>
-                    <li><a href="send-a-drive.html">Send a Drive</a></li>
-                    <li><a href="advice-and-price.html">Advice and Price</a></li>
-                    <li><a href="contact.php">Contact Us</a></li>
-                </ul>
-                <div class="clear"></div>
-            </div>
-            <div class="announcment">
-            	<ul>
-                	<li>
-                    	<h5 class="bold"><a href="blogdetail.html" class="txthover">Consectetur adipiscing elit. Nulla</a></h5>
-                        <p class="date">December 18th 2011</p>
-                    </li>
-                    <li>
-                    	<h5 class="bold"><a href="blogdetail.html" class="txthover">Consectetur adipiscing elit. Nulla</a></h5>
-                        <p class="date">December 18th 2011</p>
-                    </li>
-                    <li>
-                    	<h5 class="bold"><a href="blogdetail.html" class="txthover">Consectetur adipiscing elit. Nulla</a></h5>
-                        <p class="date">December 18th 2011</p>
-                    </li>
-                </ul>
-            	<a href="#" class="mlink">Announcements</a>
-            </div>
-        </div>
+        <?php div_navigation(); ?>
     </div>
     <div class="clear"></div>
     <!-- Banner -->
