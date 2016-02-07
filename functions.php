958ff353ad63f3beaa51d1581aec9b2d7810a77c
Denis Tolochko <denis.t@t3leads.com>
2016-02-07 06:08:11 -0800

    check uri

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function div_navigation() { ?>
                 <li class="current-menu-item"><a href="index.php">Home</a></li>
                 <li<?php if(trim($_SERVER['REQUEST_URI']) == "get-quote.php"){ ?>
                     class="current-menu-item"
-                <?php } ?>><a href="get-quote.php">Quote</a></li>
+                <?php }else{?>boo<?php } ?>><a href="get-quote.php">Quote</a></li>
                 <li><a href="send-a-drive.html">Send a Drive</a></li>
                 <li><a href="advice-and-price.html">Advice and Price</a></li>
                 <li><a href="contact.php">Contact Us</a></li>
