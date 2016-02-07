70b5bc2dc2851d1337557aa00dd6ba53bb5388c5
Denis Tolochko <denis.t@t3leads.com>
2016-02-07 05:18:51 -0800

    Change on slider

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL)
 include_once("./functions.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
